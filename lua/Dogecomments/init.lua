432821922411bc133de1c8c87a73ca20be73f045
Lee Wannacott <wannacottL@gmail.com>
2021-03-02 20:37:22 +1300

    Refactored to table for file types rather than if else. Added logic to set syntax based on filetype and a default comment_syntax.

--- lua/Dogecomments/init.lua
+++ lua/Dogecomments/init.lua
@@ -8,36 +8,36 @@ api = vim.api -- shorten calling the api
 
 -- file types table
 file = {}
-file["python"] = "#"
+file["python"] = "##"
+file["javascript"] = "//"
+file["lua"] = "--"
+file["text"] = "**"
 
 filetype = vim.bo.filetype
-if filetype == "javascript" then
-   comment_syntax = "//" 
-elseif filetype == "python" then
-    comment_syntax = "##"
-end
+print(filetype)
 
-print(ft)
--- print(filetype["python"])
+if file[filetype] ~= nil then
+    comment_syntax = file[filetype]
+elseif filetype[filetype] == nil then
+    default_comment_syntax = "//"
+    comment_syntax = default_comment_syntax
+end
+if type(string.len(comment_syntax)) ~= nil then
+    comment_syn_len = string.len(comment_syntax)
+end
+--
 function trim_whitespace(string)
     return string:match'^%s*(.*)'
 end
   
 function comment_line()
-
     get_line = api.nvim_get_current_line() 
-
     first_non_space_char = (string.len(get_line) - string.len(trim_whitespace(get_line)))+1
-
     leading_space = string.rep(" ",first_non_space_char-1)
-
     comment_exists = trim_whitespace(get_line.sub(get_line,first_non_space_char,first_non_space_char+1))
 
-
-    comment_syn_len = string.len(comment_syntax)
     space_after_comment = " "
     space_after_comment_len = string.len(space_after_comment)
-    --print(space_after_comment_len)
 
     if comment_exists ~= comment_syntax then
         set_line = api.nvim_set_current_line(leading_space .. comment_syntax .. space_after_comment .. get_line.sub(get_line,first_non_space_char))
@@ -53,5 +53,5 @@ return {
 -- TODO:s
 --  [~] Place comments with the indentation level.
 --  [~] Get filetype of file e.g .py, .js, .lua , etc. 
---  [] Assign comment type based of filetype.
+--  [~] Assign comment type based of filetype.
 --  [] Be able to place multiple line comments or multiline comments in visual mode.
