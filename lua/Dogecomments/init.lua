75a4f94560db1335d903c114a2395804010dc05f
Lee Wannacott <wannacottL@gmail.com>
2021-03-03 13:32:50 +1300

    Changed logic for comment_marker_exists so that the length of comment marker isnt hardcoded for comments of length 2 and can dynamically check commentmarkers of different lengths

--- lua/Dogecomments/init.lua
+++ lua/Dogecomments/init.lua
@@ -8,7 +8,7 @@ api = vim.api -- shorten calling the api
 
 -- file types table
 file = {}
-file["python"] = "##"
+file["python"] = "#"
 file["javascript"] = "//"
 file["lua"] = "--"
 file["text"] = "**"
@@ -17,28 +17,28 @@ filetype = vim.bo.filetype
 print(filetype)
 
 if file[filetype] ~= nil then
-    comment_syntax = file[filetype]
+    comment_marker = file[filetype]
 elseif filetype[filetype] == nil then
     default_comment_syntax = "//"
-    comment_syntax = default_comment_syntax
+    comment_marker = default_comment_syntax
 end
-if type(string.len(comment_syntax)) ~= nil then
-    comment_syn_len = string.len(comment_syntax)
+if type(string.len(comment_marker)) ~= nil then
+    length_of_comment_marker = string.len(comment_marker)
 end
 --
 function comment_line()
     get_line = api.nvim_get_current_line() 
     first_non_space_char = string.find(get_line, '%S')
     leading_space = string.rep(" ",first_non_space_char-1)
-    comment_exists = get_line.sub(get_line,first_non_space_char,first_non_space_char+1)
+    comment_marker_exists = get_line.sub(get_line,first_non_space_char,first_non_space_char + length_of_comment_marker-1)
 
     space_after_comment = " "
     space_after_comment_len = string.len(space_after_comment)
 
-    if comment_exists ~= comment_syntax then
-        set_line = api.nvim_set_current_line(leading_space .. comment_syntax .. space_after_comment .. get_line.sub(get_line,first_non_space_char))
-    elseif comment_exists == comment_syntax  then
-        set_line = api.nvim_set_current_line(leading_space .. get_line.sub(get_line, first_non_space_char + comment_syn_len + space_after_comment_len))
+    if comment_marker_exists ~= comment_marker then
+        set_line = api.nvim_set_current_line(leading_space .. comment_marker .. space_after_comment .. get_line.sub(get_line,first_non_space_char))
+    elseif comment_marker_exists == comment_marker  then
+        set_line = api.nvim_set_current_line(leading_space .. get_line.sub(get_line, first_non_space_char + length_of_comment_marker + space_after_comment_len))
     end
     end
 
@@ -50,4 +50,6 @@ return {
 --  [~] Place comments with the indentation level.
 --  [~] Get filetype of file e.g .py, .js, .lua , etc. 
 --  [~] Assign comment type based of filetype.
+
+--  [] Single line comment for normal mode
 --  [] Be able to place multiple line comments or multiline comments in visual mode.
