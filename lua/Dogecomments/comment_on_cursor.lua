6167c82586e46dd1ec22afb7e3e998396fec5948
Lee Wannacott <wannacottL@gmail.com>
2021-03-09 02:03:39 +1300

    Refactored variable names

--- lua/Dogecomments/comment_on_cursor.lua
+++ lua/Dogecomments/comment_on_cursor.lua
@@ -8,13 +8,14 @@ local function comment_on_cursor()
         get_cursor_row = api.nvim_win_get_cursor(0)[1]
         get_cursor_col = api.nvim_win_get_cursor(0)[2]
         get_line = api.nvim_get_current_line()
-        to_end_line = get_line.sub(get_line,get_cursor_col +1)
-        comment_line_on_cursor = api.nvim_set_current_line(get_line.sub(get_line,0,get_cursor_col) .. " " .. comment_marker .. space_after_comment .. to_end_line)
+        after_cursor_to_eol = get_line.sub(get_line,get_cursor_col +1)
+        to_cursor = get_line.sub(get_line,0, get_cursor_col)
+        comment_line_on_cursor = api.nvim_set_current_line(to_cursor .. " " .. comment_marker .. space_after_comment .. after_cursor_to_eol)
 
         -- '<,'> 
         --api.nvim_paste(comment_line_on_cursor,false,-1)
         --api.nvim_put(get_line,"c",true,true)
-        -- put = api.nvim_put(api.readlines,"l",true,true)
+        --api.nvim_put(block,"c",true,true)
 
     --end
 end
